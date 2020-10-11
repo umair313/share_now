3d2876233706a022b973078de08c93ffede205e8
Aun Syed Shah <aunsyedsyed@gmail.com>
2020-10-11 15:10:31 +0500

    session with login functionality

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template, url_for, request, redirect
+from flask import Flask, render_template, url_for, request, redirect, session
 
 app = Flask(__name__) 
-
+app.secret_key = "sharenow"
 @app.route('/')
 def index():
 	return render_template("index.html")
@@ -9,13 +9,20 @@ def index():
 @app.route('/login', methods=['POST', 'GET'])
 def login():
 	if request.method == "POST":
+		# getting data from form
 		userEmail = request.form["userEmail"]
-		return redirect(url_for("dashboard", usr=userEmail))
+		# adding data to session
+		session["userEmail"] = userEmail
+		return redirect(url_for("dashboard"))
 	else:
 		return render_template("login.html")
 
-@app.route('/dashboard/<usr>')
-def dashboard(usr = "aun"):
-	return render_template("dashboard.html", usr=usr)
+@app.route('/dashboard')
+def dashboard():
+	if "userEmail" in session:
+		userEmail = session["userEmail"]
+		return render_template("dashboard.html", usr=userEmail)
+	else:
+		return redirect(url_for("login"))
 if __name__ == '__main__': 
 	app.run(debug=True) 
